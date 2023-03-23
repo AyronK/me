062a802ea0e693c1f570fac49a2b2e6300281b92
AyronK <26150059+AyronK@users.noreply.github.com>
2023-03-23 22:17:21 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# me
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/remix-run-remix-pvsuvj)
---
+++ app/routes/index.tsx
@@ -0,0 +1,7 @@
+export default function Index() {
+  return (
+    <main className="flex items-center justify-center min-h-screen">
+      Soon you'll replace me with Sanity Content
+    </main>
+  );
+}
